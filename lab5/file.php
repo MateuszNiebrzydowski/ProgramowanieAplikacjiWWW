591ba0183d9a5f8653d4b1c3d77c6922cbfbe083
MateuszNiebrzydowski <115549320+MateuszNiebrzydowski@users.noreply.github.com>
2024-11-13 12:36:08 +0100

    v1.4

---
+++ lab5/file.php
@@ -0,0 +1,82 @@
+<?php   
+ $file_name="dane.txt";
+10     $answer=file($file_name);
+11    
+12     echo "<table border=1>\n";
+13     for($n=0; $n <= 3; $n++) {
+14       echo "<tr>";
+15        $elems=explode("|",$answer[$n]);
+16        for($i=0; $i <= 3; $i++) {
+17          echo "<td align=left>".$elems[$i]."</td>\n";
+18        }
+19        echo "<td><img src=spider".$elems[4].".gif ></td>\n";
+20        echo "</tr>\n";
+21     }
+22     echo "</table>";
+
+$file_name="dane.txt";
+10     $answer=file($file_name);
+11    
+12     echo "<table border=1>\n";
+13     for($n=0; $n <= 3; $n++) {
+14       echo "<tr>";
+15        $elems=explode("|",$answer[$n]);
+16        for($i=0; $i <= 3; $i++) {
+17          echo "<td align=left>".$elems[$i]."</td>\n";
+18        }
+19        echo "<td><img src=spider".$elems[4].".gif ></td>\n";
+20        echo "</tr>\n";
+21     }
+22     echo "</table>";
+
+
+
+
+$file_name="dane.txt";
+10     $answer=file($file_name);
+11    
+12     echo "<table border=1>\n";
+13     for($n=0; $n <= 3; $n++) {
+14       echo "<tr>";
+15        $elems=explode("|",$answer[$n]);
+16        for($i=0; $i <= 3; $i++) {
+17          echo "<td align=left>".$elems[$i]."</td>\n";
+18        }
+19        echo "<td><img src=spider".$elems[4].".gif ></td>\n";
+20        echo "</tr>\n";
+21     }
+22     echo "</table>";
+
+
+$file_name="dane.txt";
+10     $answer=file($file_name);
+11    
+12     echo "<table border=1>\n";
+13     for($n=0; $n <= 3; $n++) {
+14       echo "<tr>";
+15        $elems=explode("|",$answer[$n]);
+16        for($i=0; $i <= 3; $i++) {
+17          echo "<td align=left>".$elems[$i]."</td>\n";
+18        }
+19        echo "<td><img src=spider".$elems[4].".gif ></td>\n";
+20        echo "</tr>\n";
+21     }
+22     echo "</table>";
+
+
+$file_name="dane.txt";
+10     $answer=file($file_name);
+11    
+12     echo "<table border=1>\n";
+13     for($n=0; $n <= 3; $n++) {
+14       echo "<tr>";
+15        $elems=explode("|",$answer[$n]);
+16        for($i=0; $i <= 3; $i++) {
+17          echo "<td align=left>".$elems[$i]."</td>\n";
+18        }
+19        echo "<td><img src=spider".$elems[4].".gif ></td>\n";
+20        echo "</tr>\n";
+21     }
+22     echo "</table>";
+
+23  ?>
